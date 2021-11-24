ac4c3cc7c6da031a530c0d5355fca581bf8e98b1
Vinicius Jimenez <vinnicius.jimenez@gmail.com>
2021-11-24 17:39:32 -0300

    finished web app flask

--- controllers/sellers.py
+++ controllers/sellers.py
@@ -1,5 +1,5 @@
 from __main__ import app, database
-
+from flask import render_template
 
 @app.route('/')
 @app.route('/sellers')
@@ -11,16 +11,19 @@ def get_sellers():
     connection_cursor.execute('SELECT * FROM seller')
     data = connection_cursor.fetchall()
 
-    for datum in data: 
-        print(datum)
-
-
-    return 'vendedores'
+    return render_template('sellers.html', sellers=data)
 
 @app.route('/sellers/<id>')
 
 def find_seller(id):
-    return f'obter {id}'
+    connection = database.get_connection()
+    connection_cursor = connection.cursor()
+
+    connection_cursor.execute(f'SELECT * FROM seller WHERE Id = {id}')
+    datum = connection_cursor.fetchall()[0]
+
+
+    return render_template('seller.html', seller=datum)
 
 @app.route('/sellers/register')
 
